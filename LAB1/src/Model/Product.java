dc58a7bcf4fd3a44024c2f5a9ba880134556919f
Grace-Awuma <awuma.g@northeastern.edu>
2025-09-14 08:46:47 -0400

    Designing UI for Product and Address

---
+++ LAB1/src/Model/Product.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author grace
+ */
+public class Product {
+    String name;
+    String description;
+    String availNum;
+    String price;
+    Address manufactureAddress;
+    Address ShippingAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Address getManufactureAddress() {
+        return manufactureAddress;
+    }
+
+    public void setManufactureAddress(Address manufactureAddress) {
+        this.manufactureAddress = manufactureAddress;
+    }
+
+    public Address getShippingAddress() {
+        return ShippingAddress;
+    }
+
+    public void setShippingAddress(Address ShippingAddress) {
+        this.ShippingAddress = ShippingAddress;
+    }
+    
+    
+}
